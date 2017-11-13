839446189ae49ca3b911e3b12729388f7878c08e
Yin-Jou Huang <leslie071564@gmail.com>
2017-11-13 19:33:27 +0900

    restructure of make_sample_config and remove rawPrefix option

--- print_task.py
+++ print_task.py
@@ -8,9 +8,8 @@ import argparse
 def setArgs(parser):
     parser.add_argument('--config', action='store', dest='config_file')
     parser.add_argument('-t', '--task_file', action='store', dest='task_file')
-    parser.add_argument('-r', '--rawPrefix', action='store', dest='rawPrefix')
 
-def completeOptions(options):
+def getPrintTaskOptions(options):
     if options.config_file == None:
         sys.stderr.write('config file is required.\n')
         sys.exit()
@@ -18,14 +17,12 @@ def completeOptions(options):
     options.task_file = open(options.task_file, 'w') if options.task_file else sys.stdout
 
     config = yaml.load(open(options.config_file, 'r'))
-    if options.rawPrefix == None:
-        options.rawPrefix = config['ExpLocation']['rawPrefix']
-
-    options.tmpPrefix = config['ExpLocation']['tmpPrefix'] 
-    options.parallelPrefix = config['ExpLocation']['parallelPrefix'] 
+    options.rawPrefix = config['ExpLocation']['rawPrefix']
+    options.sampleTmpDir = config['ExpLocation']['sampleTmpDir'] 
+    options.parallelTmpDir = config['ExpLocation']['parallelTmpDir'] 
 
 def printTaskFile(options):
-    completeOptions(options)
+    getPrintTaskOptions(options)
 
     raw_dir, raw_file_prefix = os.path.dirname(options.rawPrefix), os.path.basename(options.rawPrefix)
     regex = re.compile(r'\d+')
@@ -36,13 +33,13 @@ def printTaskFile(options):
 
         fileID = regex.search(fileName).group(0)
         rawFile = "%s/%s" % (raw_dir, fileName)
-        parallelFile = "%s%s" % (options.parallelPrefix, fileID)
-        tmpFile = "%s%s" % (options.tmpPrefix, fileID)
+        parallelTmpFile = "%s/%s" % (options.parallelTmpDir, fileID)
+        sampleTmpFile = "%s/%s" % (options.sampleTmpDir, fileID)
 
-        cmd_1 = "python make_sample.py extract_sample %s --output_file %s --config %s" % (rawFile, parallelFile, options.config_file)
-        cmd_2 = "nice -n 19 sort -u -o %s %s" % (tmpFile, parallelFile)
-        cmd_3 = "echo %s done" % fileID
-        cmd = "%s && %s && %s" % (cmd_1, cmd_2, cmd_3)
+        extract_cmd = "python make_sample.py extract_sample %s --output_file %s --config %s" % (rawFile, parallelTmpFile, options.config_file)
+        sort_cmd = "nice -n 19 sort -u -o %s %s" % (sampleTmpFile, parallelTmpFile)
+        echo_cmd = "echo %s done" % fileID
+        cmd = " && ".join([extract_cmd, sort_cmd, echo_cmd])
 
         options.task_file.write("%s\n" % cmd)
 
