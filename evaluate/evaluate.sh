2bc37a54f7d97e1cc3e6d77875873bcca148e4e2
Yin-Jou Huang <leslie071564@gmail.com>
2018-03-08 13:53:09 +0900

    modify the location to save result-db

--- evaluate/evaluate.sh
+++ evaluate/evaluate.sh
@@ -4,6 +4,7 @@ config_file=$1
 expDataDir=`cat $config_file | shyaml get-value ExpLocation.expDataDir`
 expTrainDir=`cat $config_file | shyaml get-value ExpLocation.expTrainDir`
 expResultDir=`cat $config_file | shyaml get-value ExpLocation.expResultDir`
+result_db_loc=`cat $config_file | shyaml get-value ExpLocation.resultDB`
 
 type=`cat $config_file | shyaml get-value ExpSetting.type`
 
@@ -15,13 +16,11 @@ if [ "$type" = "MT" ] ; then
     test_source_file=$expDataDir/source_test.txt
     test_target_file=$expDataDir/target_test.txt
     output_file=$expResultDir/output.txt
-    result_db_loc=$expResultDir/result.sqlite
 
     python $evaluate_script -s $test_source_file -t $test_target_file -o $output_file -r $result_db_loc --knmt "${@:2}"
 elif [ "$type" = "LM" ] ; then
     test_file=$expDataDir/test.txt
     output_file=$expResultDir/output.txt
-    result_db_loc=$expResultDir/result.sqlite
 
     python $evaluate_script -s $test_file -o $output_file -r $result_db_loc --rnnlm
 fi
